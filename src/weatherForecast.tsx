eb49d3ac01f552ffc6815464575e48d68784a1a8
Barraza44 <diegobarrazapascual@hotmail.com>
2021-04-07 23:12:03 -0500

    added useEffect hook for forecast

--- src/weatherForecast.tsx
+++ src/weatherForecast.tsx
@@ -1,13 +1,42 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import WeatherInfo from "./weatherInfo";
+import myApiKey from "./apiKey";
+import IResponse from "./IResponse";
 
 // @ts-ignore
 const WeatherForecast = ({city, geoLocation}) => {
   console.log(city);
+  const [ forecast, setForecast ] = useState(
+    {temp: 0, feelsLike: 0, tempMin: 0, tempMax: 0, humidity: 0, visibility: 0, cityName: "", description: ""}
+  )
+
+  useEffect(() => {
+    if(geoLocation.lat !== null && geoLocation.lon !== null) {
+      fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${geoLocation.lat}&lon=${geoLocation.lon}&appid=${myApiKey}&units=metric`)
+        .then((response) => response.json())
+        .then((response) => {
+          const main: IResponse = response.main
+          setForecast(
+            {temp: main.temp,
+            feelsLike: main.feels_like,
+            tempMin: main.temp_min,
+            tempMax: main.temp_max,
+            humidity: main.humidity,
+            visibility: response.visibility,
+            cityName: response.name,
+            description: response.weather[0].main});
+          console.table(forecast);
+        });
+
+    }
+
+  },[city, geoLocation]);
+
+
   return (
     <main>
-      <h1>31 °C</h1>
-      <h2>Sunny in Guadalajara</h2>
+      <h1>{forecast.temp}</h1>
+      <h2>{`${forecast.description} in ${forecast.cityName}`}</h2>
       <div className="forecast">
         <p>Box</p>
       </div>
