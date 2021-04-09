b44c2476d6e9211a429b30292a7b73eee366b79c
Barraza44 <diegobarrazapascual@hotmail.com>
2021-04-08 22:57:01 -0500

    refactored component state

--- src/weatherForecast.tsx
+++ src/weatherForecast.tsx
@@ -2,14 +2,29 @@ import React, { useState, useEffect } from "react";
 import WeatherInfo from "./weatherInfo";
 import myApiKey from "./apiKey";
 import IResponse from "./IResponse";
+import FeelsLike from "./vector/FeelsLike.svg";
+import Frame from "./vector/Frame.svg";
+import Humidity from "./vector/Humidity.svg";
+import MaxTemp from "./vector/MaxTemp.svg";
+import MinTemp from "./vector/MinTemp.svg";
+import Wind from "./vector/Wind.svg";
 
 // @ts-ignore
 const WeatherForecast = ({city, geoLocation}) => {
   console.log(city);
   const [ forecast, setForecast ] = useState(
-    {feelsLike: 0, tempMin: 0, tempMax: 0, humidity: 0, visibility: 0, cityName: "", description: ""}
+    [
+      {feelsLike: 0, icon: FeelsLike},
+      {tempMin: 0, icon: MinTemp},
+      {tempMax: 0, icon: MaxTemp},
+      {humidity: 0, icon: Humidity},
+      {visibility: 0, icon: Frame},
+      {wind: 0, icon: Wind}
+    ]
   );
   const [ temperature, setTemperature ] = useState(0);
+  const [ cityName, setCityName ] = useState("");
+  const [ description, setDescription ] = useState("");
 
   let apiUri: string;
 
@@ -24,17 +39,18 @@ const WeatherForecast = ({city, geoLocation}) => {
         .then((response) => response.json())
         .then((response) => {
           const main: IResponse = response.main
-          setForecast(
-            {
-            feelsLike: main.feels_like,
-            tempMin: main.temp_min,
-            tempMax: main.temp_max,
-            humidity: main.humidity,
-            visibility: response.visibility,
-            cityName: response.name,
-            description: response.weather[0].main});
-          console.table(forecast);
+          setForecast([
+            {feelsLike: main.feels_like, icon: FeelsLike},
+            {tempMin: main.temp_min, icon: MinTemp},
+            {tempMax: main.temp_max, icon: MaxTemp},
+            {humidity: main.humidity, icon: Humidity},
+            {visibility: response.visibility, icon: Frame},
+            {wind: response.wind, icon: Wind}
+          ]);
           setTemperature(main.temp);
+          setDescription(response.weather[0].description);
+          setCityName(response.name);
+          console.log(response.name);
         });
 
   },[city, geoLocation]);
@@ -43,7 +59,7 @@ const WeatherForecast = ({city, geoLocation}) => {
   return (
     <main>
       <h1>{temperature}</h1>
-      <h2>{`${forecast.description} in ${forecast.cityName}`}</h2>
+      <h2>{`${description} in ${cityName !== "" ? cityName : city}`}</h2>
       <div className="forecast">
         <p>Box</p>
       </div>
